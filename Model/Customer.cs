b1e45cf86d4d8a2bde8cb88053274bee2fde2767
Ajikar saeed Basheer ahamed <ajikarsaeed.bashe@hcl.com>
2020-05-31 14:59:27 +0000

    Only Created the model classes Customer.cs CustomerAddress.cs Order.cs OrderedProduct.cs

---
+++ Model/Customer.cs
@@ -0,0 +1,46 @@
+﻿#region Description of Customer class
+/* Customer must have a default address
+ * Lazy loads, Customer can have Zero/multiple shipping address
+ * Lazy loads, Customer can have Zero/multiple Orderes placed
+ * */
+#endregion
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Customer
+    {
+        [Required]
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name ="Main Address")]
+        public CustomerAddress DefaultAddress { get; set; }
+
+        [ForeignKey("CustomerAddress")]
+        public int CustomerAddressID { get; set; }
+
+        public virtual ICollection<CustomerAddress> ShippingAddress { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    
+}
---
+++ Model/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Order
+    {
+        [Required]
+        public int OrderID { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        [Required]
+        public string DeliveryAddress { get; set; }
+
+        public decimal TotalSale { get; set; }
+
+        [ForeignKey("OrderedProducts")]
+        public int OrderedProductsID { get; set; }
+        
+        /*On clicking OrderID, it lazy loads the detail of the particular order*/
+        public virtual ICollection<OrderedProducts> OrderedProducts { get; set; }
+    }
+}
